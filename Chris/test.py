6a7942fa0815638fdf5de83fdcc33f21b3f4adc9
ThePraeceps <sunrob946@gmail.com>
2019-02-05 12:16:29 +0000

    Added test code for chris

---
+++ Chris/test.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+def reader(lines):
+	skiploc=-1
+	for idx, line in enumerate(lines):
+		lines[idx]=lines[idx].rstrip('\r\n')
+	for idx, line in enumerate(lines): # 0 Based Indexing
+		if skiploc == -1:
+			stripped = line.lstrip("\t")
+			print(str(idx) + ": " + stripped)
+			print("Length: " + str(len(stripped)))
+			if stripped[:6]== "REPEAT":
+				reps = stripped.lstrip("REPEAT ")
+				end=lines.index("END") # 0
+				print("Repeat found")
+				repeat(reps, lines[idx+1:end])
+				print("Skipping started")
+				skiploc=end
+		else:
+			if idx == skiploc:
+				print("Skipping stopped")
+				skiploc=-1
+
+
+def repeat(reps, lines):
+	for i in range(int(reps)):
+		reader(lines)
+
+payload = open("cmd.txt", 'r')
+lines = payload.readlines()
+reader(lines)
+payload.close()
