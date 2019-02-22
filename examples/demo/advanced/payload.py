d694596c0f909af5148edc57b57fdd69750b8d59
ThePraeceps <sunrob946@gmail.com>
2019-02-22 16:24:40 +0000

    Removed data printing

--- examples/demo/advanced/payload.py
+++ examples/demo/advanced/payload.py
@@ -49,7 +49,6 @@ def reverse_shell_listener():
 		codelines=shellcode.split("\n")
 		for line in codelines:
 			print("Sending: " + line)
-			print(data) # Print the output of the command.
 		print("Letting copy finish")
 		sleep(5)
 		print("Copy should have finished")
