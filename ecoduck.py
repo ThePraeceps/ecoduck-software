a9327ea32c75956a82eef0194168ef99a3d42225
ThePraeceps <sunrob946@gmail.com>
2019-02-23 12:09:31 +0000

    Corrected arm version

--- ecoduck.py
+++ ecoduck.py
@@ -583,7 +583,7 @@ class eco:
 		# Making sure device is a raspberry
 		if(platform.system() != "Linux"):
 			eco.onPi=False
-		if(platform.machine() != 'armv61'):
+		if(platform.machine() != 'armv6l'):
 			eco.onPi=False
 		if(os.geteuid() != 0):
 			raise Execption("Script not ran as root")
