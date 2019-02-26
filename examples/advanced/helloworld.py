806fc266a7b75b8ffea48d31ccccb7358fbd2b8b
ThePraeceps <sunrob946@gmail.com>
2019-02-26 11:02:53 +0000

    Updated payloads

--- examples/advanced/helloworld.py
+++ examples/advanced/helloworld.py
@@ -1,12 +1,16 @@
 from ecoduck import eco
 from time import sleep
-
-eco.press("LGUI+R")
+eco.press("ESC")
+sleep(1)
+eco.press("LGUI+d")
+sleep(1)
+eco.press("LGUI+r")
 sleep(1)
 eco.type("cmd")
 eco.press("ENTER")
 sleep(1)
 for i in range(3):
-	eco.type("echo Hello World")
-	eco.press("ENTER")
+        eco.type("echo Hello World")
+        eco.press("ENTER")
 eco.type("echo Done!")
+eco.press("ENTER")
